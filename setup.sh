66ab0fc6c020c7c1a2f4c41cbc30640eb226eb4a
Cephalode <git@cephalode.com>
2026-08-06 11:54:08 -0400

    fix: remove nvim from setup.sh stow list (deleted in earlier cleanup)

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-stow --target="$HOME" aerospace karabiner nvim git kitty btop cava fuzzel niri kanata keyd gtk-3 gtk-4 tmux zshrc
+stow --target="$HOME" aerospace karabiner git kitty btop cava fuzzel niri kanata keyd gtk-3 gtk-4 tmux zshrc
